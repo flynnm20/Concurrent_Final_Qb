18f8ce6e425719714dbc7221d2acb18d33e29f7a
flynnm20 <46966461+flynnm20@users.noreply.github.com>
2020-05-13 13:27:11 +0100

    Update csu33014-annual-partB-code.c

--- csu33014-annual-partB-code.c
+++ csu33014-annual-partB-code.c
@@ -55,7 +55,7 @@ int number_within_k_degrees(struct person *start, int total_people, int k)
   return count;
 }
 
-/* Explaination comments Matthew Flynn 17327199
+/* Q B 1: Explaination comments: Name: Matthew Flynn  Student Number :17327199
   In order to remove redunndency we need to remove the repeatly searching the same seen people. To do this I decided to use a modified frontier seach.
   We start at the start person. We then create a frontier which we will be adding to as the process continues. We also create an array of booleans to 
   keep track of which people have already been viewed. The start person is marked as visited and all other values in reached are marked as false.
@@ -70,7 +70,8 @@ int number_within_k_degrees(struct person *start, int total_people, int k)
   test values provided. 
     Once the new frontier has been created the old frontier memory is freed up to prevent usage of too much memory. Once that is done it enters 
   find_reachable_recursive_less_redundant with a new frontier, the steps remaining reduced by 1 and an updated reacable and frontier size. These 
-  changes mean that there is no revisiting of 
+  changes mean that there is no revisiting people already visited. 
+  Because of the frontier search the complexity as become 
 */
 void find_reachable_recursive_less_redundant(struct person **frontier, int steps_remaining,
                                              bool *reachable, int frontiersize)
@@ -135,41 +136,10 @@ int less_redundant_number_within_k_degrees(struct person *start,
   return count;
 }
 
-void find_reachable_recursive_parrallel(struct person **frontier, int steps_remaining,
-                                        bool *reachable, int frontiersize)
-{
-  // now deal with this person's acquaintances
-  if (steps_remaining > 0) // haven't reached the desired level of seperation yet.
-  {
-    struct person **newfrontier = malloc(sizeof(struct person *) * 500); // generate space for a new frontier.
-    int newFrontierSize = 0;                                             // keep track of new frontier size.
-    struct person *acquaintance;                                         // don't need to define this constantly
-    for (int j = 0; j < frontiersize; j++)                               // loop through all the elements in the frontier.
-    {
-      int num_known = person_get_num_known(frontier[j]); // get the number of acquaintances a person has
-      #pragma omp parallel private(aquaintance)
-      for (int i = 0; i < num_known; i++) // loop through all acquaintances
-      {
-        acquaintance = person_get_acquaintance(frontier[j], i); // get acquaintance pointer
-        if (reachable[person_get_index(acquaintance)] == false) // found new person which hasn't been noted.
-        {
-          reachable[person_get_index(acquaintance)] = true; // mark new users as visited. done here to remove redundant loops at the beginning of the function.
-          newfrontier[newFrontierSize] = acquaintance;      // add the new person to the new frontier
-          newFrontierSize++;                                // update the size of the frontier to reflect the new added person.
-        }
-      }
-    }
-    free(frontier);                                                                                        // free the memory of the out of date frontier.
-    find_reachable_recursive_less_redundant(newfrontier, steps_remaining - 1, reachable, newFrontierSize); // recurse until you have preformed the desired amount of iterations.
-  }
-  else
-  {
-    free(frontier); // have reached the end and just need to remove the last hanging frontier.
-  }
-}
+/* Q B 2: Explaination comments: Name: Matthew Flynn  Student Number :17327199
+  When implementing the code in parrallel there are many things to consider. 
 
-// computes the number of people within k degrees of the start person;
-// parallel version of the code
+*/
 int parallel_number_within_k_degrees(struct person *start,
                                      int total_people, int k)
 {
@@ -199,4 +169,38 @@ int parallel_number_within_k_degrees(struct person *start,
     }
   }
   return count;
+}
+
+void find_reachable_recursive_parrallel(struct person **frontier, int steps_remaining,
+                                        bool *reachable, int frontiersize)
+{
+  // now deal with this person's acquaintances
+  if (steps_remaining > 0) // haven't reached the desired level of seperation yet.
+  {
+    struct person **newfrontier = malloc(sizeof(struct person *) * 500); // generate space for a new frontier.
+    int newFrontierSize = 0;                                             // keep track of new frontier size.
+    struct person *acquaintance;                                         // don't need to define this constantly
+    for (int j = 0; j < frontiersize; j++)                               // loop through all the elements in the frontier.
+    {
+      int num_known = person_get_num_known(frontier[j]); // get the number of acquaintances a person has
+                                                         // each aquaintance is independent. However the aquantance newFrontierSize newFrontierSize and must be made private to prevent version.
+#pragma omp parallel private(aquaintance, newFrontierSize, newfrontier)
+      for (int i = 0; i < num_known; i++) // loop through all acquaintances
+      {
+        acquaintance = person_get_acquaintance(frontier[j], i); // get acquaintance pointer
+        if (reachable[person_get_index(acquaintance)] == false) // found new person which hasn't been noted.
+        {
+          reachable[person_get_index(acquaintance)] = true; // mark new users as visited. done here to remove redundant loops at the beginning of the function.
+          newfrontier[newFrontierSize] = acquaintance;      // add the new person to the new frontier
+          newFrontierSize++;                                // update the size of the frontier to reflect the new added person.
+        }
+      }
+    }
+    free(frontier);                                                                                        // free the memory of the out of date frontier.
+    find_reachable_recursive_less_redundant(newfrontier, steps_remaining - 1, reachable, newFrontierSize); // recurse until you have preformed the desired amount of iterations.
+  }
+  else
+  {
+    free(frontier); // have reached the end and just need to remove the last hanging frontier.
+  }
 }
